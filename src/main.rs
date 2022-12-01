e7162e8497239cdb854333c1d306b464545cdbcd
Clayton Egan-Wyer <clayton.eganwyer@vivint.com>
2022-12-01 10:43:28 -0700

    Removed unnecessary files.

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-/*
-    This file doesn't contain any advent of code solutions.
-    For those, check the folder for each corresponding day (day1, day2, etc.)
-    Each day will compile into it's own binary.
- */
-fn main() {
-    println!("Merry Christmas!")
-}
