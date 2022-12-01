959c2c67eb60a3a55d5fd9e2302f5f0771cf004c
Clayton Egan-Wyer <clayton.eganwyer@vivint.com>
2022-12-01 10:41:50 -0700

    Added some info to adventofcode/src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-
+/*
+    This file doesn't contain any advent of code solutions.
+    For those, check the folder for each corresponding day (day1, day2, etc.)
+    Each day will compile into it's own binary.
+ */
 fn main() {
-
+    println!("Merry Christmas!")
 }
